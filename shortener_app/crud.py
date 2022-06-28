4db51edcf4c7dbb07267c44281ff87dc77996afe
JichangJang <wlckd90@gmail.com>
2022-06-29 00:27:28 +0900

    feat: update click and deactivate

--- shortener_app/crud.py
+++ shortener_app/crud.py
@@ -21,3 +21,26 @@ def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
         .filter(models.URL.key == url_key, models.URL.is_active)
         .first()
     )
+
+
+def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+    return (
+        db.query(models.URL)
+        .filter(models.URL.secret_key == secret_key, models.URL.is_active)
+        .first()
+    )
+
+
+def update_db_clicks(db: Session, db_url: schemas.URL) -> models.URL:
+    db_url.clicks += 1
+    db.commit()
+    db.refresh(db_url)
+    return db_url
+
+
+def deactivate_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+    if db_url := get_db_url_by_secret_key(db=db, secret_key=secret_key):
+        db_url.is_active = False
+        db.commit()
+        db.refresh(db_url)
+    return db_url
